9debda297946b7fbbdd6427b67c109cc023ed50e
Rafael <rafaelsiqueira06@gmail.com>
2023-12-12 09:37:09 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # watch-go-scripts
+
+A simple Go script to monitor and restart Go processes defined in a YAML configuration file.
+
+## Overview
+
+This script checks if specified Go processes are running, and if not, restarts them. The process names and corresponding restart commands are defined in a YAML configuration file.
+
+## Usage
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/siqueiraa/watch-go-scripts.git
+   cd watch-go-scripts
+    ```
+2. Create a YAML configuration file named commands.yaml with the processes and restart commands:
+
+```bash 
+processes:
+  go_datafeeder: "./go_datafeeder"
+  go_triggers: "./go_triggers"
+```
+
+3. Run the script:
+```bash 
+go run main.go
+```
+This will check and restart the specified processes.
+
+##Configuration
+You can customize the configuration by editing the commands.yaml file.
+
+##Requirements
+Go installed on your system.
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Processes map[string]string `yaml:"processes"`
+}
+
+func loadConfig(file string) (Config, error) {
+	var config Config
+
+	yamlFile, err := os.ReadFile(file)
+	if err != nil {
+		return config, err
+	}
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
+func isProcessRunning(processName string) bool {
+	cmd := exec.Command("pgrep", "-fl", processName)
+	output, err := cmd.Output()
+
+	if err != nil {
+		return false
+	}
+
+	return len(output) > 0
+}
+
+func restartProcess(command string) {
+	fmt.Printf("Restarting process: %s\n", command)
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Start()
+
+	if err != nil {
+		fmt.Printf("Error restarting process: %v\n", err)
+	}
+}
+
+func main() {
+	configFile := "commands.yaml" // Adjust this to your YAML file name
+
+	for {
+		config, err := loadConfig(configFile)
+		if err != nil {
+			fmt.Printf("Error loading config: %v\n", err)
+			os.Exit(1)
+		}
+
+		for processName, command := range config.Processes {
+			log.Println("Checking process:", processName)
+			if !isProcessRunning(processName) {
+				restartProcess(command)
+			}
+		}
+		time.Sleep(10 * time.Second)
+	}
+}
